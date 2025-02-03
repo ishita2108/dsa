a9a4a6fab4af83dc24f0e54410d7b0a9fdeb3a89
ishita2108 <iwinner21@gmail.com>
2025-02-03 17:43:35 +0530

    LL commit

--- src/dsa/udemy/datastructures/LinkedList.java
+++ src/dsa/udemy/datastructures/LinkedList.java
@@ -64,6 +64,142 @@ length = 1;
     public void getLength() {
         System.out.println("Length: " + length);
     }
+    
+    public void append(int value) {
+    	Node newNode = new Node(value);
+    	
+    	if(length ==0) {
+    		head = newNode;
+    		tail = newNode;   		
+    	}
+    	else {
+    		tail.next = newNode;
+    		tail = newNode;
+    	}
+    	length++;
+    }
+    
+public Node removeLast(){
+	    
+	    if(length == 0) return null;
+	    
+	    Node temp = head;
+	    Node pre = head;
+	    
+	    while(temp.next != null){
+	        pre = temp;
+	        temp = temp.next;
+	    }
+	    
+	    tail = pre;
+	    tail.next = null;
+	    length--;
+	    
+	    if(length == 0){
+	        head = null;
+	        tail = null;
+	    }
+	    
+	    return temp;
+	}
+public void prepend(int value){
+    Node newNode = new Node(value);
+    if(length == 0){
+        head = newNode;
+        tail = newNode;
+    }
+    
+    newNode.next = head;
+    head = newNode;
+    length++;
+}
+
+public Node removeFirst(){
+    
+    if(length == 0) return null;
+    
+    Node temp = head;
+    head = head.next;
+    temp.next = null;
+    length--;
+    if(length==0){
+        tail = null;
+    }
+	return temp;
+}
+
+public Node get(int index){
+    
+    if(index >= length || index < 0) return null;
+    
+    Node temp = head;
+    
+    for(int i=0; i < index; i++){
+        temp = temp.next;
+    }
+    return temp;
+}
+
+public boolean set(int index, int value){
+    
+    Node temp = get(index);
+    if(temp != null){
+    temp.value = value;
+    return true;
+    }
+    return false;
+}
+
+public boolean insert(int index, int value){
+    if (index < 0 || index > length) return false;
+    if(index == 0){
+      prepend(value);
+      return true;
+    } 
+    if(index == length){
+        append(value);
+        return true;
+    }
+    else{
+        Node temp = get(index-1);
+        Node newNode = new Node(value);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+}
+
+public Node remove(int index){
+    if (index < 0 || index > length) return null;
+    if(index ==0) return removeFirst();
+    if(index == length) return removeLast();
+    else{
+        Node prev = get(index-1);
+        Node temp = prev.next;
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+    
+    
+}
+
+public void reverse(){
+    Node temp = head;
+    head = tail;
+    tail = temp;
+    Node before = null;
+    Node after = temp.next;
+    
+    for(int i =0; i< length; i++){
+        after = temp.next;
+        temp.next = before;
+        before = temp;
+        temp = after;
+    }
+}
 
 }
 
--- src/dsa/udemy/datastructures/Main.java
+++ src/dsa/udemy/datastructures/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         LinkedList myLinkedList = new LinkedList(4);
-
+        myLinkedList.append(1);
+        myLinkedList.append(2);
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
